e72568d24168185037cd85e3ee45b19fe2f7d3ac
bshongwe <dru.da.beat.hustle@gmail.com>
2025-08-29 10:30:16 +0200

    UPDATE: user management routes now require JWT, deleting user now requires ADMIN role -- 29-08-2025 -- 10:30 SAST

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { getUsers, getUser, createUser, updateUser, deleteUser } from "../controllers/userController";
 import { loginUser } from "../controllers/userController";
+import { authenticateJWT, requireRole } from "../middleware/authMiddleware";
 
 const router = Router();
 
-router.get("/", getUsers);
+router.get("/", authenticateJWT, getUsers);
 router.post("/register", createUser);
 router.post("/login", loginUser);
-router.get("/:id", getUser);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+router.get("/:id", authenticateJWT, getUser);
+router.put("/:id", authenticateJWT, updateUser);
+router.delete("/:id", authenticateJWT, requireRole("admin"), deleteUser);
 
 export default router;
